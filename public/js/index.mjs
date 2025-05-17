ac29a0eb860929e084ed60313628580a5880dad5
なおしむ <87138+naosim@users.noreply.github.com>
2025-05-18 08:40:58 +0900

    リファクタ

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -1,14 +1,18 @@
 import { h, text, app } from "https://unpkg.com/hyperapp"
 import {getContextDef} from "./repository.mjs"
-const contextDef = await getContextDef();
 
-var myapp = app({
-  init: {contextDef, todos: [], value: ""},
-  view: ({ contextDef, todos, value }) =>
-    h("main", {}, [
-      h("h1", {}, text(contextDef._displayName)),
-      h("a", {href:"./contextlist.html"}, text(`${contextDef._displayName}一覧`)),
-    ]),
+const topPage = ({title}) => 
+  h("main", {}, [
+    h("h1", {}, text(title)),
+    h("a", {href:"./contextlist.html"}, text(`${title}一覧`)),
+  ]);
+const updateTitle = (state, title) => ({...state, title})
+
+var dispatch = app({
+  init: {title:""},
+  view: topPage,
   node: document.getElementById("app"),
 })
+
+dispatch(updateTitle, (await getContextDef())._displayName);
 
